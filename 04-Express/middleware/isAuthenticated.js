68518524aaa504dec3e2e50dc5c2e4d219f61926
Denis Fortict <d.fortict@vivit.fr>
2021-04-23 16:22:32 +0200

    mise a jour authenthification

--- 04-Express/middleware/isAuthenticated.js
+++ 04-Express/middleware/isAuthenticated.js
@@ -3,12 +3,13 @@ import jwt from 'jsonwebtoken';
 export default function isAuthenticated(req, res, next) {
   //authorization: token type Bearer => Bearer ... token ...
 
-  if (undefined != req.headers.authorization) {
+  if (undefined !== req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
 
     try {
       let payload = jwt.verify(token, 'commentestvotreblanquette', { algorithm: 'HS512'});
       next();
+      return;
     } catch(e){}
 
   } else {
